9c8eeb309875a2dab39ec84a73aed6af018c57cf
M4NUK4N0 <manuela.canoc5@gmail.com>
2026-04-16 10:16:42 -0500

    Update Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -10,21 +10,17 @@ with st.sidebar:
   canvas_width = st.slider("Ancho del tablero", 300, 700, 500, 50)
   canvas_height = st.slider("Alto del tablero", 200, 600, 300, 50)
 
-
-
   drawing_mode = st.selectbox(
     "Herramientas de Dibujo:",
     ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
   )
-
-
   stroke_width = st.slider("Selecciona el ancho de la linea", 1, 30, 15)
 
   stroke_color = st.color_picker("Selecciona el color de la línea", "#FFFFFF")
 
   bg_color = st.color_picker("Color de fondo del tablero", "#000000")
 
-  canvas_result = st_canvas(
+canvas_result = st_canvas(
     fill_color = "rgba(255, 165, 0, 0.3)",
     stroke_width = stroke_width,
     stroke_color = stroke_color, 
