c617616cdc694e57f74f80aa1a82c7ed634bc237
M4NUK4N0 <manuela.canoc5@gmail.com>
2026-04-16 10:14:43 -0500

    Update Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -24,13 +24,12 @@ with st.sidebar:
 
   bg_color = st.color_picker("Color de fondo del tablero", "#000000")
 
-
-canvas_result = st_canvas(
-  fill_color = "rgba(255, 165, 0, 0.3)",
-  stroke_width = stroke_width,
-  stroke_color = stroke_color, 
-  backround_color = bg_color,
-  height = canvas_height,
-  width = canvas_width,
-  drawing_mode = drawing_mode,
-  key = f"canvas_{canvas_width}_{canvas_height}")
+  canvas_result = st_canvas(
+    fill_color = "rgba(255, 165, 0, 0.3)",
+    stroke_width = stroke_width,
+    stroke_color = stroke_color, 
+    backround_color = bg_color,
+    height = canvas_height,
+    width = canvas_width,
+    drawing_mode = drawing_mode,
+    key = f"canvas_{canvas_width}_{canvas_height}")
